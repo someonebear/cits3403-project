15ce33f956697f6920885fa782299fbf2e819aeb
Zhihao <zhi.lin1@outlook.com>
2022-05-19 03:03:35 +0800

    Register auth, views blueprints

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 
+
 def create_app():
   app = Flask(__name__)
   app.config.from_object("config.DevelopmentConfig")
-  
+
+  from .auth import auth
+  from .views import views
+
+  app.register_blueprint(views, url_prefix="/")
+  app.register_blueprint(auth, url_prefix="/")
+
   return app

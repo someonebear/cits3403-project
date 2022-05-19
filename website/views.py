54a1dfea34b511591274c2fbbcb552f75f1b00f1
Zhihao <zhi.lin1@outlook.com>
2022-05-19 17:07:46 +0800

    Added stats view

--- website/views.py
+++ website/views.py
@@ -7,3 +7,8 @@ views = Blueprint('views', __name__)
 @login_required
 def home():
   return render_template("quizzle.html", user=current_user)
+
+@views.route('stats')
+@login_required
+def stats():
+  return render_template("stats.html", user=current_user)

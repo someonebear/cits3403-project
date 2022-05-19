045f3f2f0f7f965a2e227898af703b2db476d8d0
Zhihao <zhi.lin1@outlook.com>
2022-05-19 16:49:13 +0800

    Added check log-in functionality

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -9,7 +10,7 @@ def create_app():
   app = Flask(__name__)
   app.config.from_object("config.DevelopmentConfig")
   db.init_app(app)
-  
+
   from .auth import auth
   from .views import views
 
@@ -19,6 +20,14 @@ def create_app():
   from .models import User, Questions
 
   create_database(app)
+
+  login_manager = LoginManager()
+  login_manager.login_view = 'auth.login'
+  login_manager.init_app(app)
+
+  @login_manager.user_loader
+  def load_user(id):
+    return User.query.get(int(id))
 
   return app
 
--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -15,12 +16,14 @@ def login():
     if user:
       if check_password_hash(user.password, password):
         flash("Successful log in!", category='success')
+        login_user(user, remember=True)
+        return redirect(url_for("views.home"))
       else:
         flash("Incorrect password. Try again.", category='error')
     else:
       flash("User does not exist.", category='error')
 
-  return render_template("login.html", boolean=True)
+  return render_template("login.html", user=current_user)
 
 @auth.route('/sign-up', methods=["GET", "POST"])
 def sign_up():
@@ -45,5 +48,12 @@ def sign_up():
       db.session.add(new_user)
       db.session.commit()
       flash("Account Created", category='success')
+      login_user(user, remember=True)
       return redirect(url_for("views.home"))
-  return render_template("signup.html", boolean=True)
+  return render_template("signup.html", user=current_user)
+
+  @auth.route('/logout')
+  @login_required
+  def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))

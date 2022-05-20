5031eb523203c528d58030c2ec5987da438746c6
Zhihao <zhi.lin1@outlook.com>
2022-05-20 22:08:06 +0800

    User to/from dict methods

--- website/models.py
+++ website/models.py
@@ -1,6 +1,7 @@
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
+from werkzeug.security import generate_password_hash
 
 class User(db.Model, UserMixin):
   id = db.Column(db.Integer, primary_key=True)
@@ -10,12 +11,24 @@ class User(db.Model, UserMixin):
   questions = db.relationship('Questions')
   stats = db.relationship('Stats', uselist=False)
 
+  def set_password(self, password):
+    self.password = generate_password_hash(password)
+
   def to_dict(self, include_email=False):
     data = {
       'id': self.id,
-      'username': self.username,
-      ''
+      'username': self.username
     }
+    if include_email:
+      data['email'] = self.email
+    return data
+
+  def from_dict(self, data, new_user=False):
+    for field in ['username', 'email']:
+      if field in data:
+        setattr(self, field, data[field])
+    if new_user and 'password' in data:
+      self.set_password(data['password'])
 
 class Questions(db.Model):
   id = db.Column(db.Integer, primary_key=True)

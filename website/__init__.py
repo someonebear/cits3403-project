f3f4e18245ece3d020bae7751631745a95db3582
Zhihao <zhi.lin1@outlook.com>
2022-05-20 19:25:04 +0800

    Remove flask migrate code

--- website/__init__.py
+++ website/__init__.py
@@ -2,17 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
-from flask_migrate import Migrate
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
-migrate = Migrate()
 
 def create_app():
   app = Flask(__name__)
   app.config.from_object("config.DevelopmentConfig")
   db.init_app(app)
-  migrate.init_app(app, db)  
 
   from .auth import auth
   from .views import views
